84ba11517008acf22bcbab6f5b316fe2a3bbde66
Nick-Damico <nickalan82@icloud.com>
2024-09-28 20:27:22 -0400

    feat: add ref author(user) to quizzes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_09_27_140053) do
+ActiveRecord::Schema[7.2].define(version: 2024_09_28_233025) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_27_140053) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "author_id", null: false
+    t.index ["author_id"], name: "index_quizzes_on_author_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -33,4 +35,6 @@ ActiveRecord::Schema[7.2].define(version: 2024_09_27_140053) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
+
+  add_foreign_key "quizzes", "users", column: "author_id"
 end
