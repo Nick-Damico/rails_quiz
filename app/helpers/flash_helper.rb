7b9d9fa3780162d2bedcad7c2d5272d92db02362
Nick-Damico <nickalan82@icloud.com>
2024-09-30 11:04:39 -0400

    style: apply style class to flash button

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -2,9 +2,16 @@ module FlashHelper
   def flash_class(level)
       case level
       when "notice" then "bg-green-100 text-green-800 border-green-300"
-      when "alert" then "bg-red-100 text-red-800 border-red-300"
-      when "error" then "bg-red-100 text-red-800 border-red-300"
+      when "alert", "error" then "bg-red-100 text-red-800 border-red-300"
       else "bg-blue-100 text-blue-800 border-blue-300"
       end
+  end
+
+  def flash_button_class(level)
+    case level
+    when "notice" then "bg-green-500"
+    when "alert", "error" then "bg-red-500"
+    else "bg-blue-500"
+    end
   end
 end
