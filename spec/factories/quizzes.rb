903d2f413591b50fb6be63ec5dedef00de737c93
Nick-Damico <nickalan82@icloud.com>
2024-10-18 10:04:44 -0400

    factory: add required author_id to quizzes

--- spec/factories/quizzes.rb
+++ spec/factories/quizzes.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :quiz do
+    author { create(:user) }
     name { "#{Faker::Lorem.sentence(word_count: 3)} Quiz" }
     description { Faker::Lorem.paragraph }
   end
