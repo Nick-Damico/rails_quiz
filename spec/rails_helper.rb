e5fe873b533038e7584e3f541855e408c6e74d86
Nick-Damico <nickalan82@icloud.com>
2024-10-19 10:02:23 -0400

    chore(spec): add Devise integration helpers

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -68,6 +68,10 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  # Devise Integration Helpers
+  config.include Devise::Test::IntegrationHelpers
+  config.include Devise::Test::ControllerHelpers, type: :view
 end
 
 
