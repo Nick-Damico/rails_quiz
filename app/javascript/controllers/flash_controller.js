aeae1dd101d1dce2b93b55b94df4a9f085246a98
Nick-Damico <nickalan82@icloud.com>
2024-10-29 11:32:48 -0400

    style: better display of multiple error msgs

--- app/javascript/controllers/flash_controller.js
+++ app/javascript/controllers/flash_controller.js
@@ -1,17 +1,19 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from "@hotwired/stimulus";
 
 // Connects to data-controller="flash"
 export default class extends Controller {
   connect() {
-    this.removeOnTimeout(2500)
+    this.removeOnTimeout(2500);
   }
 
   onClick(e) {
-    this.removeOnTimeout()
+    this.removeOnTimeout();
   }
 
   removeOnTimeout(delay = 0) {
-    this.element.addEventListener('animationend', () => this.element.remove());
-    setTimeout(() => { this.element.classList.add('animate-fadeOut') }, delay);
+    this.element.addEventListener("animationend", () => this.element.remove());
+    setTimeout(() => {
+      this.element.classList.add("animate-fadeOut");
+    }, delay);
   }
 }
