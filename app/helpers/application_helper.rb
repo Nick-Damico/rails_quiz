ca47c67c49d22520c9b3214bf80f12192a6ec937
Nick-Damico <nickalan82@icloud.com>
2024-11-09 09:02:39 -0500

    feat: add #back_button helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def back_button(path:, text: nil)
+    btn_text = text || t("buttons.back")
+
+    link_to btn_text, path, class: "inline-flex bg-primary text-white px-2 py-2 mb-4"
+  end
 end
