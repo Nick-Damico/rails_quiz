1b95059a4ca21313a8f6d74fa4ca1113bb0521bd
Nick-Damico <nickalan82@icloud.com>
2024-11-14 11:56:23 -0500

    chore(factory): add trait :with_answers to quiz

--- spec/factories/quizzes.rb
+++ spec/factories/quizzes.rb
@@ -3,5 +3,11 @@ FactoryBot.define do
     author { create(:user) }
     title { "#{Faker::Lorem.sentence(word_count: 3)} Quiz" }
     description { Faker::Lorem.paragraph }
+
+    trait :with_answers do
+     after(:create) do |quiz, evaluator|
+       create_list(:answer, 3, quiz: quiz)
+     end
+   end
   end
 end
