dbdee1d827bd47d4f638244b9a774522473fed5d
Nick-Damico <nickalan82@icloud.com>
2024-11-19 11:39:39 -0500

    refactor(helper): replace back_button with partial
    Having a helper for this felt a bit 'clunky'. Replaced with a partial
    that takes a single argument of :path for the link.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,10 +4,4 @@ module ApplicationHelper
       yield if block_given?
     end
   end
-
-  def back_button(path:, text: nil)
-    btn_text = text || t("buttons.back")
-
-    link_to btn_text, path, class: "inline-flex bg-primary text-white px-2 py-2 mb-4"
-  end
 end
