dcef504476d35a6a4ee1929159745e7c8647760b
Nick-Damico <nickalan82@icloud.com>
2024-12-05 01:26:17 -0500

    validate: dependent destroy of question choices

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,6 +1,6 @@
 class Question < ApplicationRecord
   belongs_to :quiz
-  has_many :choices, class_name: "Question::Choice"
+  has_many :choices, class_name: "Question::Choice", dependent: :destroy
 
   validates_presence_of(:quiz)
   validates_presence_of(:content)
--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -2,5 +2,9 @@ FactoryBot.define do
   factory :question do
     quiz { create(:quiz) }
     content { Faker::Lorem.sentence(word_count: 5) }
+
+    after(:create) do |question, evaluator|
+      create_list(:choice, 1,  question:)
+    end
   end
 end
