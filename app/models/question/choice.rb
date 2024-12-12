cdc7d02113b1c7cfa9e6f90b7b4f1e294708ecb8
Nick-Damico <nickalan82@icloud.com>
2024-12-12 15:51:56 -0500

    fix: invalid url_helpers generated from form_with

--- app/models/question/choice.rb
+++ app/models/question/choice.rb
@@ -1,7 +1,16 @@
 class Question::Choice < ApplicationRecord
+  extend ActiveModel::Naming
   belongs_to :question
 
   validates_presence_of(:question)
   validates_presence_of(:content)
   validates :correct, inclusion: [ true, false ]
+
+  # INFO: This prevents form_with url helpers
+  # from generating incorrect routes like: question_question_choices_*.
+  # This was the suggested work-around for dealing with the above issue when
+  # using nested routes/models.
+  def self.model_name
+    ActiveModel::Name.new(self, Question)
+  end
 end
