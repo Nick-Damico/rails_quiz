70bdea4e04fc8e6fd7de900e31bb2b1e5de13305
Nick-Damico <nickalan82@icloud.com>
2025-01-03 16:28:19 -0500

    refactor: #display_field_error
    This now handles and properly displays a line by line output of multiple
    errors on a single field.

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,17 +1,23 @@
 module FormHelper
-  def field_with_error_icon(model, field)
-    return if model.errors[field.to_sym].none?
-
-    render "icons/exclamation_triangle"
-  end
-
   def display_field_error(model, field)
     field_sym = field.to_sym
     return unless model.errors[field_sym].present?
 
-    content_tag(:p, class: "flex items-center text-sm text-red-400 mt-1") do
-      concat field_with_error_icon(model, field_sym)
-      concat model.errors[field_sym].join(", ")
+    safe_join(model.errors[field].map do |error|
+      field_with_error_msg(error)
+    end)
+  end
+
+  private
+
+  def field_with_error_msg(error)
+    content_tag(:p, class: "flex items-center text-xs text-red-400 mt-1") do
+      concat field_with_error_icon
+      concat error.capitalize
     end
+  end
+
+  def field_with_error_icon
+    render "icons/exclamation_triangle"
   end
 end
