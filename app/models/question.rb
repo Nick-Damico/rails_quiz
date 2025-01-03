de138ee334c8b9f524752d56c0cc4219ea3d6c9e
Nick-Damico <nickalan82@icloud.com>
2025-01-03 16:30:00 -0500

    feat: add choices validations to question
    Valid Questions require:
      1. at least 2 choices for a question
      2. one must be marked as correct

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,6 +4,22 @@ class Question < ApplicationRecord
 
   validates_presence_of(:quiz)
   validates_presence_of(:content)
+  validate :validate_number_of_choices
+  validate :validate_correct_choice
 
   accepts_nested_attributes_for :choices
+
+  private
+
+  def validate_number_of_choices
+    return unless choices.size <= 1
+
+    errors.add(:base, "must have at least 2 choices")
+  end
+
+  def validate_correct_choice
+    return unless choices.none?(&:correct)
+
+    errors.add(:base, "must have one choice marked as correct")
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,4 +6,36 @@ RSpec.describe Question, type: :model do
 
   it { should belong_to(:quiz) }
   it { should have_many(:choices) }
+
+  context "#validate_number_of_choices" do
+    it 'when fewer than 2 choices' do
+      invalid_question = Question.new(content: "What is 2 + 2?", quiz: create(:quiz))
+
+      invalid_question.choices << build(:choice, correct: true)
+
+      expect(invalid_question).to be_invalid
+      expect(invalid_question.errors[:base]).to include("must have at least 2 choices")
+    end
+
+    it 'when more than 2 choices' do
+      valid_question = Question.new(content: "What is 2 + 2?", quiz: create(:quiz))
+
+      valid_question.choices << build(:choice)
+      valid_question.choices << build(:choice, correct: true)
+
+      expect(valid_question).to be_valid
+    end
+  end
+
+  context "#validate_correct_choice" do
+    it 'when a correct choice not marked' do
+      invalid_question = Question.new(content: "What is 2 + 2?", quiz: create(:quiz))
+
+      invalid_question.choices << build(:choice, correct: false)
+      invalid_question.choices << build(:choice, correct: false)
+
+      expect(invalid_question).to be_invalid
+      expect(invalid_question.errors[:base]).to include("must have one choice marked as correct")
+    end
+  end
 end
