b5fe0e1cf0694cf30e89f215f568de08cb918bf5
Nick-Damico <nickalan82@icloud.com>
2025-01-08 13:32:06 -0500

    chore(style): remove ugly bg color from banner

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def banner_heading(&block)
-    content_tag(:div, class: "bg-gray-500 p-4") do
+    content_tag(:div, class: "border border-gray-700 p-4") do
       yield if block_given?
     end
   end
