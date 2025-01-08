31cc73c58de8889475e700556d8b383dae95e835
Nick-Damico <nickalan82@icloud.com>
2025-01-08 13:06:31 -0500

    refactor:#breadcrumb_item support none-link crumbs

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -4,6 +4,10 @@ module BreadcrumbHelper
                   locals: { klass: "size-4 ml-1" }
     content = safe_join([ breadcrumb.name, content_tag(:span, icon) ])
 
-    link_to(content, breadcrumb.path, class: "px-1 py-2 flex items-center hover:underline group")
+    if breadcrumb.path
+      link_to(content, breadcrumb.path, class: "px-1 py-2 flex items-center hover:underline group")
+    else
+      content_tag(:span, content, class: "px-1 py-2 flex items-center")
+    end
   end
 end
