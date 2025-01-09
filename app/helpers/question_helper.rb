c49cb4bc35c37f86d59141c4c534fe920e509bd5
Nick-Damico <nickalan82@icloud.com>
2025-01-09 00:01:23 -0500

    feat: add prev/next question buttons to preview
    Calculating next and previous question may move to the model at some
    point... for now, it lives here.

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -5,4 +5,14 @@ module QuestionHelper
 
     ("a".."z").to_a[num]
   end
+
+  def prev_question_id(question, ids)
+    ids[ids.index(question.id) - 1]
+  end
+
+  def next_question_id(question, ids)
+    next_possible_id = ids.index(question.id) + 1
+
+    ids[next_possible_id % ids.length]
+  end
 end
