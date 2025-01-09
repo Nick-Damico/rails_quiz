b91dc44711ee8b8b281ec6c2a281ee574991f798
Nick-Damico <nickalan82@icloud.com>
2025-01-09 09:57:47 -0500

    fix: truncate long breadcrumbs

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -2,7 +2,7 @@ module BreadcrumbHelper
   def breadcrumb_item(breadcrumb)
     icon = render partial: "icons/chevron_right",
                   locals: { klass: "size-4 ml-1" }
-    content = safe_join([ breadcrumb.name, content_tag(:span, icon) ])
+    content = safe_join([ breadcrumb.name.truncate(20), content_tag(:span, icon) ])
 
     if breadcrumb.path
       link_to(content, breadcrumb.path, class: "px-1 py-2 flex items-center hover:underline group")
