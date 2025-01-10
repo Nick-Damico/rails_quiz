d499d2c3496d53a9db65d8e5743b6e025981818e
Nick-Damico <nickalan82@icloud.com>
2025-01-10 12:39:17 -0500

    fix(migration): swap precision and scale values

--- db/migrate/20250110162514_create_answer_sheets.rb
+++ db/migrate/20250110162514_create_answer_sheets.rb
@@ -3,7 +3,7 @@ class CreateAnswerSheets < ActiveRecord::Migration[7.2]
     create_table :answer_sheets do |t|
       t.references :quiz, null: false, foreign_key: true
       t.references :user, null: false, foreign_key: true
-      t.decimal :grade, scale: 5, precision: 2
+      t.decimal :grade, precision: 5, scale: 2
 
       t.timestamps
     end
