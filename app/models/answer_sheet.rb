9d4239452432e9952bba392da9eb32afdd70e4ce
Nick-Damico <nickalan82@icloud.com>
2025-01-11 23:05:42 -0500

    feat: add assoc. answer_sheet_questions
    This record is used to store the users answer to the associated
    question.

--- app/models/answer_sheet.rb
+++ app/models/answer_sheet.rb
@@ -1,4 +1,6 @@
 class AnswerSheet < ApplicationRecord
   belongs_to :quiz
   belongs_to :user
+
+  has_many :answer_sheet_questions
 end
--- spec/models/answer_sheet_spec.rb
+++ spec/models/answer_sheet_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 RSpec.describe AnswerSheet, type: :model do
   it { should belong_to(:user) }
   it { should belong_to(:quiz) }
+  it { should have_many(:answer_sheet_questions) }
 end
