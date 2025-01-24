d7049839e31972309c1f3ae7d195720e72547d39
Nick-Damico <nickalan82@icloud.com>
2025-01-24 17:15:58 -0500

    feat(spec): test rounding & validate answer_sheet

--- spec/models/answer_sheet/grader_spec.rb
+++ spec/models/answer_sheet/grader_spec.rb
@@ -26,6 +26,29 @@ RSpec.describe AnswerSheet::Grader, type: :model do
       expect(answer_sheet.grade).to eq(50.00)
     end
 
+    it "rounds the grade to two decimal places" do
+      quiz = create(:quiz, questions_count: 3)
+      answer_sheet = create(:answer_sheet, :with_zero_score, quiz:)
+      first_answer_sheet_question = answer_sheet.answer_sheet_questions.first
+      first_answer_sheet_question.update(
+        answer: first_answer_sheet_question.question.choices.find(&:correct)
+      )
+
+      described_class.new(answer_sheet).grade
+
+      expect(answer_sheet.grade).to eq(33.33)
+    end
+  end
+
+  context "with incomplete answer_sheet" do
+    it "raises an ArgumentError" do
+      answer_sheet = create(:answer_sheet)
+      answer_sheet.prepare
+
+      expect {
+        described_class.new(answer_sheet)
+      }.to raise_error(ArgumentError, "Answer sheet must be complete")
+    end
   end
 
   context "without answer_sheet" do
@@ -35,7 +58,7 @@ RSpec.describe AnswerSheet::Grader, type: :model do
       }.to raise_error(ArgumentError, "Answer sheet cannot be nil or empty")
     end
 
-    it "raises an ArgumentError if not an AnwserSheet" do
+    it "raises an ArgumentError if not an AnswerSheet" do
       wrong_argument = create(:quiz)
 
       expect {
