fa9a3f61c60d6412abb2cb02828cfaa30d156967
Nick-Damico <nickalan82@icloud.com>
2025-01-26 09:32:06 -0500

    feat(spec): add feature user completes a full quiz

--- spec/features/answer_sheet_questions/show_spec.rb
+++ spec/features/answer_sheet_questions/show_spec.rb
@@ -57,6 +57,22 @@ RSpec.describe "AnswerSheetQuestion show page", type: :feature do
 
     click_button "Next"
 
-    expect(page).to have_content(I18n.t("flash.answer_sheet_questions.update.success"))
+  scenario "user can complete a full a quiz", js: true do
+    answer_sheet.answer_sheet_questions.each.with_index(1) do |answer_sheet_question, index|
+      last_question = index == answer_sheet.answer_sheet_questions.count
+      choice = answer_sheet_question.question.choices.first
+
+      visit answer_sheet_question_path(answer_sheet_question)
+
+      find("label[for='answer_sheet_question_answer_id_#{choice.id}']").click
+
+      if last_question
+        click_button "Finish"
+      else
+        click_button "Next"
+      end
+    end
+
+    expect(page).to have_current_path(answer_sheet_path(answer_sheet))
   end
 end
