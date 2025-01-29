8e032f34be355851d2333e80383cf534f5c5a842
Nick-Damico <nickalan82@icloud.com>
2025-01-29 12:24:36 -0500

    refactor(helper): #breadcrumb_helper
    Improvements:
      - Extract contents to private methods where possible.
      - Remove trailing '>' icon from the current path or last breadcrumb in
        trail.
      - Optimize icon render to a single render call using memoization.

--- app/helpers/breadcrumb_helper.rb
+++ app/helpers/breadcrumb_helper.rb
@@ -1,13 +1,25 @@
 module BreadcrumbHelper
   def breadcrumb_item(breadcrumb)
-    icon = render partial: "icons/chevron_right",
-                  locals: { klass: "size-4 ml-1" }
-    content = safe_join([ breadcrumb.name.truncate(20), content_tag(:span, icon) ])
+    content = build_content(breadcrumb)
 
     if breadcrumb.path
       link_to(content, breadcrumb.path, class: "px-1 py-2 flex items-center underline group")
     else
       content_tag(:span, content, class: "px-1 py-2 flex items-center")
     end
+  end
+
+  private
+
+  def build_content(breadcrumb)
+    if current_page?(breadcrumb.path)
+      breadcrumb.name.truncate(20)
+    else
+      safe_join([ breadcrumb.name.truncate(20), content_tag(:span, icon) ])
+    end
+  end
+
+  def icon
+    @icon ||= render partial: "icons/chevron_right", locals: { klass: "size-4 ml-1" }
   end
 end
