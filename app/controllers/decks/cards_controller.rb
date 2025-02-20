ef364cc3ab85c779d6e22d049ace11909f7e0f3b
Nick-Damico <nickalan82@icloud.com>
2025-02-20 17:05:37 -0500

    feat: add breadcrumbs to card actions

--- app/controllers/decks/cards_controller.rb
+++ app/controllers/decks/cards_controller.rb
@@ -1,6 +1,7 @@
 class Decks::CardsController < ApplicationController
   before_action :set_deck, only: %i[new]
   before_action :set_card, only: %i[show]
+  before_action :set_breadcrumbs, only: %i[new]
 
   def new
     @card = authorize(@deck.cards.new)
@@ -14,6 +15,14 @@ class Decks::CardsController < ApplicationController
 
     def set_card
       @card = Decks::Card.find(params[:id])
+    end
+
+    def set_breadcrumbs
+      add_breadcrumb("Design")
+      add_breadcrumb("Decks", author_decks_path)
+      if @deck
+        add_breadcrumb(@deck.title, author_deck_path(@deck))
+      end
     end
 
     def set_deck
