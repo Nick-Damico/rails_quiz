04eb0b76c50df48d0da63adb321bcc742e18f8fc
Nick-Damico <nickalan82@icloud.com>
2025-02-24 12:10:39 -0500

    chore: helper #banner_heading use <header> tag

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def banner_heading(&block)
-    content_tag(:div, class: "border border-gray-700 p-4") do
+    content_tag(:header, class: "border border-gray-700 p-4") do
       yield if block_given?
     end
   end
