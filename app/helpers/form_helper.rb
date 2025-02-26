4a7cdb37a5141a1c6efb16b731e262416bb1bbce
Nick-Damico <nickalan82@icloud.com>
2025-02-26 16:55:12 -0500

    refactor: #display_field_error accept options hash
    This will allow extending the default class styles for the error
    <p> tag.

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,23 +1,26 @@
 module FormHelper
-  def display_field_error(model, field)
+  def display_field_error(model, field, options = {})
     field_sym = field.to_sym
     return unless model.errors[field_sym].present?
 
     safe_join(model.errors[field].map do |error|
-      field_with_error_msg(error)
+      field_with_error_msg(error, options)
     end)
   end
 
   private
 
-  def field_with_error_msg(error)
-    content_tag(:p, class: "flex items-center text-xs text-red-400 mt-1") do
-      concat field_with_error_icon
-      concat error.capitalize
-    end
-  end
+    def field_with_error_msg(error, options = {})
+      classes = "flex items-center text-xs text-red-400 mt-1"
+      classes << " #{options[:class]}" if options[:class].present?
 
-  def field_with_error_icon
-    render "icons/exclamation_triangle"
-  end
+      content_tag(:p, class: classes) do
+        concat field_with_error_icon
+        concat error.capitalize
+      end
+    end
+
+    def field_with_error_icon
+      render "icons/exclamation_triangle"
+    end
 end
