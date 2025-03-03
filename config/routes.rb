e70713cd650f9ef40e0521fe1492ad0e1384f6f9
Nick-Damico <nickalan82@icloud.com>
2025-03-03 15:16:34 -0500

    chore: organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,17 +13,9 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "home#index"
 
-  namespace :dashboard do
-    resources :users, only: %i[show update]
-  end
-
   namespace :author do
     resources :decks
     resources :quizzes
-  end
-
-  resources :decks, only: %i[] do
-    resources :cards, shallow: true, module: :decks
   end
 
   resources :answer_sheets, only: %i[create destroy show] do
@@ -34,6 +26,14 @@ Rails.application.routes.draw do
   end
 
   resources :answer_sheet_questions, only: %i[show update]
+
+  namespace :dashboard do
+    resources :users, only: %i[show update]
+  end
+
+  resources :decks, only: %i[] do
+    resources :cards, shallow: true, module: :decks
+  end
 
   resources :quizzes, only: %i[index show] do
     resources :questions, only: %i[create edit destroy new show update]
