01b677504dd9a423d9e41f4ffba69ef97c9f8dba
Nick-Damico <nickalan82@icloud.com>
2025-03-03 15:35:08 -0500

    feat: add decks_controller#index with policy
    Added:
      - route decks_path
      - DeckController and DeckController#index action
      - DeckPolicy & DeckPolicy::Scope
      - updated navbar with drop-down Study link for decks_path

---
+++ app/policies/deck_policy.rb
@@ -0,0 +1,12 @@
+class DeckPolicy < ApplicationPolicy
+  class Scope
+    def initialize(user, scope)
+      @user = user
+      @scope = scope
+    end
+
+    def resolve
+      @scope.all
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,7 @@ Rails.application.routes.draw do
     resources :users, only: %i[show update]
   end
 
-  resources :decks, only: %i[] do
+  resources :decks, only: %i[index] do
     resources :cards, shallow: true, module: :decks
   end
 
