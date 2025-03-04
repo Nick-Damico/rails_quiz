217c666a512e021b74bb916ac5d5acfa308f8f3f
Nick-Damico <nickalan82@icloud.com>
2025-03-04 17:54:37 -0500

    refactor: Quiz#time_to_complete returns seconds
        - update #time_to_complete to use  ActiveSupport::Duration to calculate time in seconds
        - add DurationHelper#seconds_to_minutes to convert duration for output
        - update deck_spec.rb

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -5,13 +5,12 @@ class Quiz < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :author
 
-  SECONDS_IN_MIN = 60.0
   SECONDS_PER_QUESTION = 30.0
 
   # Future update with allow the Author to set the
   # estimated time per question. This will allow for more or
   # less time based on question difficulty.
   def time_to_complete
-    (questions.count * SECONDS_PER_QUESTION / SECONDS_IN_MIN).round
+    ActiveSupport::Duration.build(questions.count * SECONDS_PER_QUESTION).to_i
   end
 end
--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -7,25 +7,16 @@ RSpec.describe Quiz, type: :model do
   it { should have_many(:questions) }
 
   context "#time_to_complete" do
-    it "displays estimated time to complete" do
-      quiz = build(:quiz, questions_count: 2)
-
-      est_time = quiz.time_to_complete
-
-      expect(est_time).to_not be_nil
-      expect(est_time).to be_a(Integer)
-    end
-
-    it "estimates 30 secs per question" do
+    it "returns the total estimated time to complete all cards in seconds" do
       quiz = create(:quiz, questions_count: 2)
 
-      expect(quiz.time_to_complete).to eq(1)
+      expect(quiz.time_to_complete).to eq(60)
     end
 
-    it "rounds up to the nearest whole number" do
-      quiz = create(:quiz, questions_count: 3)
+    it "returns 0 with zero questions" do
+      quiz = create(:quiz, questions_count: 0)
 
-      expect(quiz.time_to_complete).to eq(2)
+      expect(quiz.time_to_complete).to eq(0)
     end
   end
 end
