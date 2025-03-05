65ec62e675a2eb640dccd1495afb1a5d0abc913f
Nick-Damico <nickalan82@icloud.com>
2025-03-05 17:10:18 -0500

    chore(spec): add has_many authored_decks check

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  it { should have_many(:authored_decks) }
   it { should have_many(:authored_quizzes) }
   it { should have_many(:user_decks) }
 
