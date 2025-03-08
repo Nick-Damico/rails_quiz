e4cb30914473269a5729791e20fb5e9fdbcf714e
Nick-Damico <nickalan82@icloud.com>
2025-03-08 11:45:43 -0500

    feat: destroy dependent user_deck_card records

--- app/models/user_deck.rb
+++ app/models/user_deck.rb
@@ -2,7 +2,7 @@ class UserDeck < ApplicationRecord
   belongs_to :user
   belongs_to :deck
 
-  has_many :user_deck_cards
+  has_many :user_deck_cards, dependent: :destroy
   has_many :cards, through: :user_deck_cards
 
   delegate :cards, to: :deck, allow_nil: true
