ecbdea84d604a3ee88d4087494faf3af6485e976
Nick-Damico <nickalan82@icloud.com>
2025-03-09 22:06:44 -0400

    refactor: DRY up repeated logic w/CollectionHelper
    Changes:
      - add UserDeckHelper
      - refactor QuestionHelper using CollectionHelper methods

--- app/helpers/question_helper.rb
+++ app/helpers/question_helper.rb
@@ -1,4 +1,6 @@
 module QuestionHelper
+  include CollectionHelper
+
   def num_to_letter(num)
     num = num.to_i
     return if num.negative?
@@ -6,21 +8,11 @@ module QuestionHelper
     ("a".."z").to_a[num]
   end
 
-  def question_index(question, ids)
-    ids.index(question.id)
-  end
-
-  def question_number(question, ids)
-    ids.index(question.id) + 1
-  end
-
   def prev_question_id(question, ids)
-    ids[question_index(question, ids) - 1]
+    ids[id_index(question.id, ids) - 1]
   end
 
   def next_question_id(question, ids)
-    next_possible_id = question_index(question, ids) + 1
-
-    ids[next_possible_id % ids.length]
+    next_id(question.id, ids)
   end
 end
