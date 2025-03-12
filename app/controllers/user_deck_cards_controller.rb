335ef3e230fbf6880fde298e5432102148ef47cf
Nick-Damico <nickalan82@icloud.com>
2025-03-12 15:26:15 -0400

    feat(wip): setup user_deck_cards controller #show

--- app/controllers/user_deck_cards_controller.rb
+++ app/controllers/user_deck_cards_controller.rb
@@ -1,6 +1,7 @@
 class UserDeckCardsController < ApplicationController
   before_action :set_user_deck_card, only: [ :show ]
   def show
+    # TODO: Set instance variables for show view partial rendering of review_card
     @user_deck_card = authorize(@user_deck_card)
     respond_to do |format|
       format.html { redirect_to user_deck_url(@user_deck_card.user_deck, card_id: @user_deck_card.id) }
