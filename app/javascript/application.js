68eadfc9d1842277108f365916e46cdb190d578c
Nick-Damico <nickalan82@icloud.com>
2025-03-15 22:57:41 -0400

    feat(js): add helpers/ and pin them
    Add:
      - helpers/html_helper.js includes commonly repeated operations.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,5 +1,6 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers";
+import "helpers/html_helper"
 
 import "chartkick";
 import "chartkick-bundle";
