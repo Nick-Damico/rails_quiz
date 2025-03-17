a61fa56c3272e690a81f557bfeac4b71cddb8c00
Nick-Damico <nickalan82@icloud.com>
2025-03-17 13:19:35 -0400

    feat(js): add #addClass, #removeClass helpers
    Move these common tasks to a helper.

--- app/javascript/helpers/html_helper.js
+++ app/javascript/helpers/html_helper.js
@@ -1,7 +1,4 @@
-/*
- * @param {HTMLElement} element
- * @return void
- */
+/* PUBLIC HELPERS */
 function removeDisabledAttribute(element) {
   if (_elementHasAttribute(element, "disabled")) {
     _removeAttribute(element, "disabled");
@@ -14,6 +11,19 @@ function removeHiddenClass(element) {
   }
 }
 
+function addClass(element, className) {
+  if (_elementHasClass(element, className)) return;
+
+  _addClass(element, className);
+}
+
+function removeClass(element, className) {
+  if (_elementHasClass(element, className)) {
+    _removeClass(element, className);
+  }
+}
+
+/* PRIVATE HELPERS */
 function _elementHasAttribute(element, attribute) {
   return element.hasAttribute(attribute);
 }
@@ -26,8 +36,13 @@ function _elementHasClass(element, className) {
   return element.classList.contains(className);
 }
 
+function _addClass(element, className) {
+  element.classList.add(className);
+}
+
 function _removeClass(element, className) {
   element.classList.remove(className);
 }
 
-export { removeDisabledAttribute, removeHiddenClass };
+/* EXPORT METHODS */
+export { addClass, removeClass, removeDisabledAttribute, removeHiddenClass };
