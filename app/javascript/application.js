640f3e05df86b6f2019cdea53f31a179c7dd2764
Nick-Damico <nickalan82@icloud.com>
2025-03-17 12:48:05 -0400

    feat(js): add helpers/string_helper.js

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,6 +1,7 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "controllers";
-import "helpers/html_helper"
+import "helpers/html_helper";
+import "helpers/string_helper";
 
 import "chartkick";
 import "chartkick-bundle";
