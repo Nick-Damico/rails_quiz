cc83a8879a13865b6804cc1799ce3ee436e72714
Nick-Damico <nickalan82@icloud.com>
2025-03-29 11:29:37 -0400

    refactor: #group_by_user_deck returns AR relation

--- app/models/user_deck_card.rb
+++ app/models/user_deck_card.rb
@@ -2,7 +2,8 @@ class UserDeckCard < ApplicationRecord
   belongs_to :user_deck
   belongs_to :card, class_name: "Decks::Card"
 
-  scope :group_by_rating, ->(user_deck) { where(user_deck_id: user_deck.id).group(:card_rating).count }
+  scope :by_user_deck, ->(user_deck) { where(user_deck_id: user_deck.id) }
+  scope :group_by_rating, ->(user_deck) { by_user_deck(user_deck).group(:card_rating) }
 
   delegate :deck, to: :user_deck
 
--- spec/models/user_deck_card_spec.rb
+++ spec/models/user_deck_card_spec.rb
@@ -13,4 +13,16 @@ RSpec.describe UserDeckCard, type: :model do
       expect(user_deck_card.not_rated?).to be(true)
     end
   end
+
+  describe ".group_by_rating" do
+    it "groups the cards by rating" do
+      user_deck = create(:user_deck, :with_user_deck_cards)
+      user_deck.user_deck_cards.first.correct!
+      user_deck.user_deck_cards.last.incorrect!
+
+      result = UserDeckCard.group_by_rating(user_deck).count
+
+      expect(result).to eq({ "correct" => 1, "incorrect" => 1 })
+    end
+  end
 end
