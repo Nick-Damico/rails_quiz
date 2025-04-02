8459bad7beca6605c2521fb041a72c4cf0217bdc
Nick-Damico <nickalan82@icloud.com>
2025-04-02 10:40:28 -0400

    fix: capybara cannot find button by text

--- spec/features/dashboard/update_user_info_spec.rb
+++ spec/features/dashboard/update_user_info_spec.rb
@@ -2,14 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Dashboard User Updates Information', type: :feature do
   scenario 'User changes username' do
-      user = create(:user)
-      sign_in user
-      new_username = Faker::Name.unique
+    user = create(:user)
+    sign_in user
+    new_username = Faker::Name.unique
 
-      visit dashboard_user_path(user)
+    visit dashboard_user_path(user)
 
-      fill_in 'Username', with: new_username
+    fill_in 'Username', with: new_username
 
-      click_button 'Update'
+    click_button 'Update User'
   end
 end
