141c1f383dd144d03501484b83dceaa5d3208fc1
Nick-Damico <nickalan82@icloud.com>
2025-05-16 21:49:21 -0400

    remove(model): study plan name uniqueness

--- app/models/study_plan.rb
+++ app/models/study_plan.rb
@@ -2,6 +2,5 @@ class StudyPlan < ApplicationRecord
   belongs_to :user
 
   validates :name, presence: true
-  validates :name, uniqueness: { case_sensitive: true }
   validates :description, presence: true
 end
