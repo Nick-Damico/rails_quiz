3d071b8089793c1fd2e493e8c56e8c7e5a0c8555
Nick-Damico <nickalan82@icloud.com>
2025-05-21 11:02:36 -0400

    refactor(helper): initialize as a Hash
    We want to memoize the named partials within the Hash. This will insure
    that the correct icon is render on subsequent calls.

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -2,6 +2,7 @@ module IconHelper
   def render_icon(partial_name, options = {})
     icon_class = options[:klass] ? "#{options[:klass]}" : "size-6"
 
-    @render_icon ||= render("icons/#{partial_name}", klass: icon_class)
+    @render_icon ||= {}
+    @render_icon[partial_name.to_sym] ||= render("icons/#{partial_name}", klass: icon_class)
   end
 end
