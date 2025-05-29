b9acc6f36819b7701a18fe60269a1d289ec979f8
Nick-Damico <nickalan82@icloud.com>
2025-05-29 10:40:08 -0400

    refactor(helper):  @render_icon to @render_map

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -1,17 +1,17 @@
 module IconHelper
   def render_icon(partial_name, options = {})
     icon_class = options[:klass] || "size-6"
-    @render_icon ||= {}
+    @render_map ||= {}
 
-    cached = @render_icon[partial_name.to_sym]
+    cached = @render_map[partial_name.to_sym]
 
     if cached.nil? || cached[:klass] != icon_class
-      @render_icon[partial_name.to_sym] = {
+      @render_map[partial_name.to_sym] = {
         klass: icon_class,
         html: render("icons/#{partial_name}", klass: icon_class)
       }
     end
 
-    @render_icon[partial_name.to_sym][:html]
+    @render_map[partial_name.to_sym][:html]
   end
 end
