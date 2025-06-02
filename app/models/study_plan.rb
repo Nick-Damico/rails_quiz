6bf6e04e4ba30d9ca0b065ddf4953fb4a92edb46
Nick-Damico <nickalan82@icloud.com>
2025-06-02 19:00:10 -0400

    feat: add quizzes to study_plans

--- app/models/study_plan.rb
+++ app/models/study_plan.rb
@@ -2,6 +2,7 @@ class StudyPlan < ApplicationRecord
   belongs_to :user
 
   has_many :study_plan_decks, dependent: :destroy
+  has_many :study_plan_quizzes, dependent: :destroy
 
   validates :name, presence: true
   validates :description, presence: true
---
+++ app/models/study_plan_quiz.rb
@@ -0,0 +1,4 @@
+class StudyPlanQuiz < ApplicationRecord
+  belongs_to :study_plan
+  belongs_to :quiz
+end
