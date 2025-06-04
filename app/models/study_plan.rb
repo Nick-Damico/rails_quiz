0b024b156cf61c9194079fae4224b1957eceed8e
Nick-Damico <nickalan82@icloud.com>
2025-06-04 11:20:09 -0400

    feat: add assoc. methods to study_plan
    Change:
      - add #quizzes
      - add #decks

--- app/models/study_plan.rb
+++ app/models/study_plan.rb
@@ -2,7 +2,10 @@ class StudyPlan < ApplicationRecord
   belongs_to :user
 
   has_many :study_plan_decks, dependent: :destroy
+  has_many :decks, through: :study_plan_decks
+
   has_many :study_plan_quizzes, dependent: :destroy
+  has_many :quizzes, through: :study_plan_quizzes
 
   validates :name, presence: true
   validates :description, presence: true
