edc104f5d52697c9b9835024f4d494d83f9e0871
Nick-Damico <nickalan82@icloud.com>
2025-06-13 21:55:39 -0400

    feat: allow users to fetch study_plans
    change:
      - Add Users::StudyPlans controller w/policy

--- app/controllers/users/study_plans_controller.rb
+++ app/controllers/users/study_plans_controller.rb
@@ -1,5 +1,13 @@
 class Users::StudyPlansController < ApplicationController
-  def index; end
+  before_action :set_user, only: %i[index]
+
+  def index
+    @study_plans = policy_scope([ :users, @user.study_plans ])
+  end
 
   private
+
+    def set_user
+      @user = User.find(params[:user_id])
+    end
 end
