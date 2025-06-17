39e5b4e5d87c5a16f165c66bca531e31e704c029
Nick-Damico <nickalan82@icloud.com>
2025-06-16 21:15:32 -0400

    feat: add breadcrumbs to "decks_controller#index"

--- app/controllers/decks_controller.rb
+++ app/controllers/decks_controller.rb
@@ -1,17 +1,23 @@
 class DecksController < ApplicationController
   before_action :set_deck, only: %i[show]
+  before_action :set_breadcrumbs, only: %i[index show]
   def index
     @decks = policy_scope(Deck)
   end
 
   def show
-    add_breadcrumb "Study"
-    add_breadcrumb "Decks", decks_path
-    add_breadcrumb @deck.title, nil
     @deck = authorize(@deck)
   end
 
   private
+
+    def set_breadcrumbs
+      add_breadcrumb "Study"
+      add_breadcrumb "Decks", decks_path
+      if @deck.present?
+        add_breadcrumb @deck.title, nil
+      end
+    end
 
     def set_deck
       @deck = Deck.find(params[:id])
