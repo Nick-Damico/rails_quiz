4758f42efb3ab8affbd58918fbf223d190b97ff8
Nick-Damico <nickalan82@icloud.com>
2025-07-13 22:53:49 -0400

    feat(style): detect drag events on OS files

--- app/javascript/controllers/users/avatar_uploader_controller.js
+++ app/javascript/controllers/users/avatar_uploader_controller.js
@@ -1,18 +1,54 @@
 import { Controller } from "@hotwired/stimulus";
 
+const iconStyles = {
+  dropZone: {
+    active: ["border-emerald-500"],
+    inactive: ["border-gray-300"],
+  },
+  icon: {
+    active: ["stroke-emerald-500"],
+    inactive: ["stroke-gray-500"],
+  },
+};
+
 // Connects to data-controller="users--avatar-uploader"
 export default class extends Controller {
-  static targets = ["dropzone", "fileInput", "preview"];
+  static targets = ["dropzone", "fileInput", "icon", "preview"];
 
   connect() {
     console.log("Avatar Uploader Controller connected");
   }
 
   onBrowse(event) {
-    console.log("Browse button clicked");
     this.fileInputTarget.click();
   }
 
-  onDragOver(event) {}
-  onDrop(event) {}
+  onDragOver(event) {
+    this._toggleDropzoneStyles("enter");
+  }
+
+  onDragLeave(event) {
+    this._toggleDropzoneStyles("leave");
+  }
+
+  onDrop(event) {
+    console.log("file dropped");
+    console.log(event)
+  }
+
+  _toggleDropzoneStyles(action) {
+    if (action === "enter") {
+      this.dropzoneTarget.classList.add(...iconStyles.dropZone.active);
+      this.dropzoneTarget.classList.remove(...iconStyles.dropZone.inactive);
+
+      this.iconTarget.classList.add(...iconStyles.icon.active);
+      this.iconTarget.classList.remove(...iconStyles.icon.inactive);
+    } else if (action === "leave") {
+      this.dropzoneTarget.classList.remove(...iconStyles.dropZone.active);
+      this.dropzoneTarget.classList.add(...iconStyles.dropZone.inactive);
+
+      this.iconTarget.classList.remove(...iconStyles.icon.active);
+      this.iconTarget.classList.add(...iconStyles.icon.inactive);
+    }
+  }
 }
