5637bccad96b25dd7eeb7e85dca731b0b6a9692c
Nick-Damico <nickalan82@icloud.com>
2025-07-20 10:42:07 -0400

    style: display avatar with UserHelper#avatar

--- app/helpers/dashboard/users_helper.rb
+++ app/helpers/dashboard/users_helper.rb
@@ -1,2 +1,11 @@
 module Dashboard::UsersHelper
+  def avatar(user)
+    return unless user
+
+    if user.avatar.present?
+      image_tag current_user.avatar, class: "w-8 h-8 rounded-full border-2 border-gray-500 hover:border-emerald-500 transition-border ease-in-out duration-500"
+    else
+      content_tag :div, class: "w-8 h-8 rounded-full bg-emerald-400"
+    end
+  end
 end
