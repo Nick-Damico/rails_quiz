691f85373bd6ae7c42c28042c857eb6cffc88aa8
Nick-Damico <nickalan82@icloud.com>
2025-08-14 13:06:17 -0400

    feat: basic filter of quiz questions on index pg

--- app/javascript/controllers/questions/search_controller.js
+++ app/javascript/controllers/questions/search_controller.js
@@ -5,6 +5,25 @@ export default class extends Controller {
   static targets = ["input", "question"];
 
   connect() {
-    console.log("Questions/Search Controller connected");
+    this._addEventListeners();
+  }
+
+  onInput(e) {
+    console.log("Search input changed:", e.target.value);
+    if (!this.hasQuestionTarget) return;
+
+    const searchValue = e.target.value.toLowerCase().trim();
+
+    for (let question of this.questionTargets) {
+      let questionText = question.textContent.toLowerCase().trim();
+      if (questionText.includes(searchValue)) { 
+        question.classList.remove("animate-fadeOut");
+        // question.classList.remove("opacity-0");
+        continue;
+      }
+
+      question.classList.add("animate-fadeOut");
+    }
+  }
   }
 }
