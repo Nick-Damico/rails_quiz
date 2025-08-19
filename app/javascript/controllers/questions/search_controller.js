4f4ab54049d98266e86633f9783c49daf41e7888
Nick-Damico <nickalan82@icloud.com>
2025-08-18 21:05:21 -0400

    feat: debounce on text filter search

--- app/javascript/controllers/questions/search_controller.js
+++ app/javascript/controllers/questions/search_controller.js
@@ -1,4 +1,5 @@
 import { Controller } from "@hotwired/stimulus";
+import debounce from "helpers/debounce";
 import { addClass, removeClass } from "helpers/html_helper";
 
 // Connects to data-controller="questions--search"
@@ -7,25 +8,54 @@ export default class extends Controller {
 
   connect() {
     this._addEventListeners();
+    this.debouncedFilter = debounce((text) => {
+      this.filterOnInput(text);
+    }, 600);
+  }
+
+  saveInput(){
+    console.log('Saving data');
   }
 
   onInput(e) {
+    this.debouncedFilter(e.target.value)
+  }
+
+  filterOnInput(text) {
     if (!this.hasQuestionTarget) return;
 
-    const searchValue = e.target.value.toLowerCase().trim();
+    const searchValue = text.toLowerCase().trim();
 
     for (let question of this.questionTargets) {
       let questionText = question.textContent.toLowerCase().trim();
       let classList = [...question.classList];
 
       if (questionText.includes(searchValue) && classList.includes("hidden")) { 
-          removeClass(question, "hidden");
-        }
+        removeClass(question, "hidden");
+      }
 
       if (!questionText.includes(searchValue)) {
         addClass(question, "hidden");
       }
     }
   }
+
+  _addEventListeners() {
+    // for (let question of this.questionTargets) {
+    //   question.addEventListener("transitionend", (event) => {
+    //     console.log("Transition ended:", event.propertyName);
+    //     if (event.propertyName === "opacity") {
+    //       if ([...event.target.classList].includes("opacity-0")) { 
+    //         console.log("has opacity-0");
+    //         addClass(event.target, "hidden");
+    //       } else {
+    //         removeClass(event.target, "hidden");
+    //       }
+    //     }
+
+        // console.log(event.propertyName)
+        // question.classList.add("hidden");
+      // }, { once: true });
+    // }
   }
 }
