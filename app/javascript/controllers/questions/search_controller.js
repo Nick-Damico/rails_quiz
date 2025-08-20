05be0a9e59ec2353b5de0367a8c91b45e5f4070f
Nick-Damico <nickalan82@icloud.com>
2025-08-20 07:24:56 -0400

    fix: rename search_contr* to search_filter_contr*

--- app/javascript/controllers/questions/search_controller.js
+++
@@ -1,9 +0,0 @@
-import SearchFilter from "controllers/search_filter";
-import { addClass, removeClass } from "helpers/html_helper";
-
-// Connects to data-controller="questions--search"
-export default class extends SearchFilter {
-  onInput(e) {
-    this.debouncedFilter(e.target.value)
-  }
-}
---
+++ app/javascript/controllers/search_filter_controller.js
@@ -0,0 +1,38 @@
+import { Controller } from "@hotwired/stimulus"
+import { addClass, removeClass } from "helpers/html_helper";
+import { normalize } from "helpers/string_helper";
+import debounce from "helpers/debounce";
+
+export default class extends Controller {
+  static targets = ["item"];
+
+  connect() {
+    this.debouncedFilter = debounce((text) => {
+      this.filter(text);
+    })
+  }
+
+  onInput(evt) {
+    this.debouncedFilter(evt.target.value)
+  }
+
+  filter(text) {
+    if (!this.hasItemTarget) return;
+
+    const searchValue = normalize(text);
+
+    // TODO: Filter only on whole words
+    for (let item of this.itemTargets) {
+      let itemText = normalize(item.textContent);
+      let classList = [...item.classList];
+
+      if (itemText.includes(searchValue) && classList.includes("hidden")) { 
+        removeClass(item, "hidden");
+      }
+
+      if (!itemText.includes(searchValue)) {
+        addClass(item, "hidden");
+      }
+    }
+  }
+}
