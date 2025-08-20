ddcc14d3894f0c4372f9a7df00cac59624dd0eb2
Nick-Damico <nickalan82@icloud.com>
2025-08-20 07:24:08 -0400

    feat(helper): add #normalize string helper

--- app/javascript/helpers/string_helper.js
+++ app/javascript/helpers/string_helper.js
@@ -2,4 +2,8 @@ function capitalize(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export { capitalize };
+function normalize(string) {
+  return string.toLowerCase().trim();
+}
+
+export { capitalize, normalize };
