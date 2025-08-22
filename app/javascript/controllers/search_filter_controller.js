79a31c3acc10ef87ea6e6e2ad1be9ae9fe63cf48
Nick-Damico <nickalan82@icloud.com>
2025-08-22 09:58:37 -0400

    feat(js): display empty msg on failed search

--- app/javascript/controllers/search_filter_controller.js
+++ app/javascript/controllers/search_filter_controller.js
@@ -1,15 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
-import { addClass, removeClass } from "helpers/html_helper";
+import { Controller } from "@hotwired/stimulus";
+import { addClass, removeClass, isHidden } from "helpers/html_helper";
 import { normalize } from "helpers/string_helper";
 import debounce from "helpers/debounce";
 
 export default class extends Controller {
-  static targets = ["item"];
+  static targets = ["item", "itemContainer", "emptyMessage"];
 
   connect() {
     this.debouncedFilter = debounce((text) => {
       this.filter(text);
-    })
+    });
   }
 
   onInput(evt) {
@@ -26,13 +26,35 @@ export default class extends Controller {
       let itemText = normalize(item.textContent);
       let classList = [...item.classList];
 
-      if (itemText.includes(searchValue) && classList.includes("hidden")) { 
+      if (itemText.includes(searchValue) && classList.includes("hidden")) {
         removeClass(item, "hidden");
       }
 
       if (!itemText.includes(searchValue)) {
         addClass(item, "hidden");
       }
+
+      if (this._containerIsEmpty()) {
+        this._displayEmptyMessage();
+      } else {
+        this._hideEmptyMessage();
+      }
     }
   }
+
+  _containerIsEmpty() {
+    return this.items().every((item) => isHidden(item));
+  }
+
+  _displayEmptyMessage() {
+    removeClass(this.emptyMessageTarget, 'hidden')
+  }
+
+  _hideEmptyMessage() {
+    addClass(this.emptyMessageTarget, 'hidden')
+  }
+
   items() {
+    return this.itemTargets;
+  }
+}
