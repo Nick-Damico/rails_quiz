4f7ab497cdf7938d25be5a14406e2fabf1c1c904
Nick-Damico <nickalan82@icloud.com>
2025-09-03 11:24:26 -0400

    fix: display of avatar icon - user w/o profile img

--- app/helpers/dashboard/users_helper.rb
+++ app/helpers/dashboard/users_helper.rb
@@ -5,7 +5,7 @@ module Dashboard::UsersHelper
     if user.avatar.present?
       image_tag current_user.avatar, class: "w-8 h-8 rounded-full border-2 border-gray-500 hover:border-emerald-500 transition-border ease-in-out duration-500"
     else
-      content_tag :div, class: "w-8 h-8 rounded-full bg-emerald-400"
+      content_tag :div, "", class: "w-8 h-8 rounded-full bg-emerald-400"
     end
   end
 end
