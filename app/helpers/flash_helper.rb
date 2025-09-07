0cef4ba8f49cd8811605e73fe23b666562e88568
Nick-Damico <nickalan82@icloud.com>
2025-09-07 01:00:41 -0400

    style: format ruby code

--- app/helpers/flash_helper.rb
+++ app/helpers/flash_helper.rb
@@ -1,10 +1,10 @@
 module FlashHelper
   def flash_class(level)
-      case level
-      when "notice" then "bg-green-100 text-green-800 border-green-300"
-      when "alert", "error" then "bg-red-100 text-red-800 border-red-300"
-      else "bg-blue-100 text-blue-800 border-blue-300"
-      end
+    case level
+    when "notice" then "bg-green-100 text-green-800 border-green-300"
+    when "alert", "error" then "bg-red-100 text-red-800 border-red-300"
+    else "bg-blue-100 text-blue-800 border-blue-300"
+    end
   end
 
   def flash_button_class(level)
