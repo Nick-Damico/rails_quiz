b711f236e6223f5174421933590ee0177848af48
Nick-Damico <nickalan82@icloud.com>
2025-09-24 17:18:02 -0400

    fix(factory): make user factory confirmed
    change:
      - `confirmable` Devise module added recently making existing user
        factories unauthenticated for login.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,6 +3,7 @@ FactoryBot.define do
     email { Faker::Internet.email }
     username { Faker::Name.name }
     password { Faker::Internet.password(min_length: 8, max_length: 8, mix_case: true, special_characters: true) }
+    confirmed_at { Time.current }
 
     after(:build) do |user, evaluator|
       user.password_confirmation = user.password
@@ -11,5 +12,9 @@ FactoryBot.define do
 
   trait :with_bio do
     bio { Faker::Lorem.paragraph }
+  end
+
+  trait :unconfirmed do
+    confirmed_at { nil }
   end
 end
