67908ae4c37916781c4726e198429871d41646e7
Nick-Damico <nickalan82@icloud.com>
2025-10-02 15:51:04 -0400

    feat: setup user study_plan new route/action/views

--- spec/requests/users/study_plans_spec.rb
+++ spec/requests/users/study_plans_spec.rb
@@ -8,7 +8,16 @@ RSpec.describe "StudyPlans", type: :request do
     sign_in user
   end
 
+  # GET :index
   describe "GET /study_plans" do
+    it "returns a list of study plans" do
+      get user_study_plans_path(user_id: user.id)
+      expect(response).to have_http_status(:ok)
+    end
+  end
+
+  # GET :new
+  describe "GET /users/:user_id/study_plans/new" do
     it "returns a list of study plans" do
       get user_study_plans_path(user_id: user.id)
       expect(response).to have_http_status(:ok)
