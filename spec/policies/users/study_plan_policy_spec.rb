826baf9c67977b7a8fb9e2dc379b98f5823a1f8c
Nick-Damico <nickalan82@icloud.com>
2025-10-09 23:17:15 -0400

    feat(spec): setup study_plan policies

---
+++ spec/policies/users/study_plan_policy_spec.rb
@@ -0,0 +1,24 @@
+require "rails_helper"
+
+RSpec.describe Users::StudyPlanPolicy, type: :policy do
+  subject { described_class }
+
+  let(:user) { create(:user) }
+  let(:study_plans) { create_list(:study_plan, 2, user: user) }
+
+  permissions "Scope#resolve" do
+    it "returns all study_plans" do
+      expect(described_class::Scope.new(user, StudyPlan).resolve).to contain_exactly(*study_plans)
+    end
+  end
+
+  permissions :show? do
+    it "permits authenticated user access" do
+      expect(subject).to permit(user, study_plans.first)
+    end
+
+    it "prevents unauthenticated user access" do
+      expect(subject).to_not permit(build(:user), study_plans.first)
+    end
+  end
+end
