5767c107af2051e001f75c19791a223394a9811e
Nick-Damico <nickalan82@icloud.com>
2025-10-09 21:24:54 -0400

    feat(study_plan): add action/view for show route

--- app/controllers/users/study_plans_controller.rb
+++ app/controllers/users/study_plans_controller.rb
@@ -1,5 +1,6 @@
 class Users::StudyPlansController < ApplicationController
   before_action :set_user, only: %i[index new create]
+  before_action :set_study_plan, only: %i[show]
 
   def index
     @study_plans = policy_scope([ :users, @user.study_plans ])
@@ -19,6 +20,9 @@ class Users::StudyPlansController < ApplicationController
     end
   end
 
+  def show
+    @study_plan = authorize([ :users, @study_plan ])
+  end
 
   private
     def study_plan_params
@@ -27,5 +31,9 @@ class Users::StudyPlansController < ApplicationController
 
     def set_user
       @user = User.find(params[:user_id])
+    end
+
+    def set_study_plan
+      @study_plan = StudyPlan.find(params[:id])
     end
 end
