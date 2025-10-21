73e7114ae0b0b3075bc0adf897260b50391f1196
Nick-Damico <nickalan82@icloud.com>
2025-10-20 21:35:24 -0400

    feat(wip): add quiz to users study_plan
    change:
      - working version
      - needs error handling
      - further testing

--- app/controllers/study_plans/quizzes_controller.rb
+++ app/controllers/study_plans/quizzes_controller.rb
@@ -1,5 +1,26 @@
-class StudyPlans::QuizzesController < ApplicationController
-  def create; end
+module StudyPlans
+  class QuizzesController < ApplicationController
+    before_action :set_study_plan
 
-  def destroy; end
+    def create
+      @study_plan = authorize(@study_plan, policy_class: Users::StudyPlanPolicy)
+      quiz = Quiz.find(quiz_params[:quiz_id])
+      @study_plan.quizzes << quiz unless @study_plan.quizzes.include?(quiz)
+
+      flash.now[:notice] = "Quiz was successfully added to your study plan."
+      redirect_back(fallback_location: user_study_plan_path(current_user, @study_plan))
+    end
+
+    def destroy; end
+
+    private
+
+      def quiz_params
+        params.require(:quiz).permit(:quiz_id)
+      end
+
+      def set_study_plan
+        @study_plan = StudyPlan.find(params[:study_plan_id])
+      end
+  end
 end
--- spec/requests/study_plans/quizzes_spec.rb
+++ spec/requests/study_plans/quizzes_spec.rb
@@ -1,10 +1,28 @@
 require "rails_helper"
 
 RSpec.describe "StudyPlans::QuizzesController", type: :request do
-  let!(:study_plan) { create(:study_plan) }
+  let!(:user) { create(:user) }
+  let!(:study_plan) { create(:study_plan, user:) }
+  let!(:quiz) { create(:quiz) }
+
+  before { sign_in user }
 
   # POST /study_plans/:study_plan_id/quizzes
-  describe "POST /create" do; end
+  describe "POST /create" do
+    context "with valid params" do
+      it "responds with HTTP status 302(:found)" do
+        post study_plan_quizzes_path(study_plan), params: { quiz: { quiz_id: quiz.id } }
+
+        expect(response).to have_http_status(:found)
+      end
+
+      it "adds the quiz to the study plan" do
+        expect {
+          post study_plan_quizzes_path(study_plan), params: { quiz: { quiz_id: quiz.id } }
+        }.to change { study_plan.quizzes.count }.by(1)
+      end
+    end
+  end
 
   # DELETE /study_plans/:study_plan_id/quizzes/:id
   describe "DELETE /destroy" do; end
