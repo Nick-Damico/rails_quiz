9d03bcda8e0dc946bc0867933ad8be7c65ca8941
Nick-Damico <nickalan82@icloud.com>
2026-05-20 10:40:46 -0400

    fix: remove forced flex alignment from card text
    Allow multiline and future rich text formatting without
    imposing centered alignment by default.

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -4,9 +4,7 @@ module CardHelper
   end
 
   def card_text_tag(text, wrapping_tag: :p)
-    classes = %w[
-      flex items-center justify-center whitespace-pre-line text-md pt-2
-    ]
+    classes = %w[ flex whitespace-pre-line text-md pt-2 ]
 
     content_tag(wrapping_tag.to_sym, class: classes) do
       text
