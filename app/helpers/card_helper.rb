5d2b93359c24206773e3bb391512d2f02747eb55
Nick-Damico <nickalan82@icloud.com>
2026-05-20 10:34:54 -0400

    feat(helper): tag wrapper for card content
    change:
      - improve output that gives user ability to add newlines and returns
        to flashcard content.

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -2,4 +2,14 @@ module CardHelper
   def new_card?(card)
     !!card.id
   end
+
+  def card_text_tag(text, wrapping_tag: :p)
+    classes = %w[
+      flex items-center justify-center whitespace-pre-line text-md pt-2
+    ]
+
+    content_tag(wrapping_tag.to_sym, class: classes) do
+      text
+    end
+  end
 end
