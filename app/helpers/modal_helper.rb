59bce849e7aaba3124da3ee63f13bf5b51b8dd2b
Nick-Damico <nickalan82@icloud.com>
2026-07-09 10:10:22 -0400

    refactor(modal): build form fields from passed object

---
+++ app/helpers/modal_helper.rb
@@ -0,0 +1,36 @@
+module ModalHelper
+  def hidden_field_for_modal(form, object)
+    field = "#{object.class.name.downcase}_id"
+    form.hidden_field(field, value: object.id)
+  end
+
+  def model_for_modal(object)
+    class_name = object.class.name
+
+    if class_name == "Deck"
+      StudyPlanDeck.new(deck: object)
+    elsif class_name == "Quiz"
+      StudyPlanQuiz.new(quiz: object)
+    end
+  end
+
+  def radio_btn_disabled?(radio_btn, object)
+    class_name = object.class.name
+
+    if class_name == "Deck"
+      radio_btn.object.decks.include?(object)
+    elsif class_name == "Quiz"
+      radio_btn.object.quizzes.include?(object)
+    end
+  end
+
+  def url_for_modal(object)
+    class_name = object.class.name
+
+    if class_name == "Deck"
+      study_plan_decks_path
+    elsif class_name == "Quiz"
+      study_plan_quizzes_path
+    end
+  end
+end
