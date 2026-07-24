7000c39bad6bc1397e98f3876946d56ffcefa3fd
Nick-Damico <nickalan82@icloud.com>
2026-07-24 11:24:27 -0400

    feat(deck): add supporting methods for publishing

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -8,8 +8,27 @@ class Deck < ApplicationRecord
 
   validates :title, presence: true
 
+  PUBLISHABLE_CARD_COUNT = 5
+
+  def publish!
+    update_column(:published_at, Time.current) if publishable?
+  end
+
+  def publishable?
+    cards.count >= PUBLISHABLE_CARD_COUNT
+  end
+
+  def published?
+    published_at.present?
+  end
+
   def time_to_complete
     avg_time_per_card = 15
     ActiveSupport::Duration.build(cards.count * avg_time_per_card).to_i
+  end
+
+  # NOTE: Eventually this will need some clean-up. We will need to notify users that had this as part of their study_plans.
+  def unpublish!
+    update_column(:published_at, nil)
   end
 end
--- spec/models/deck_spec.rb
+++ spec/models/deck_spec.rb
@@ -8,6 +8,52 @@ RSpec.describe Deck, type: :model do
 
   it { should validate_presence_of(:title) }
 
+  context "#publish!" do
+    let!(:deck) { create(:deck, card_count: 5) }
+    it "publishes a publishale quiz by setting the published_at datetime" do
+      deck = create(:deck, card_count: 5)
+
+      expect {
+        deck.publish!
+      }.to change(deck, :published_at).from(nil)
+    end
+
+    it "does not publish an unpublishable quiz" do
+      deck = create(:deck, card_count: 0)
+
+      expect {
+      deck.publish!
+    }.not_to change(deck, :published_at)
+    end
+  end
+
+  context "#published?" do
+    it "returns false if published_at is nil" do
+      deck = build(:deck, published_at: nil)
+
+      expect(deck).not_to be_published
+    end
+
+    it "returns true if published_at is present" do
+      deck = build(:deck, published_at: Time.current)
+
+      expect(deck).to be_published
+    end
+  end
+
+  context "#publishable?" do
+    it "returns false if the quiz has less than 5 cards" do
+      deck = create(:deck, card_count: 4)
+
+      expect(deck).not_to be_publishable
+    end
+
+    it "returns true if the quiz has 5 or more cards" do
+      deck = create(:deck, card_count: 5)
+
+      expect(deck).to be_publishable
+    end
+  end
   describe "#time_to_complete" do
     it "returns the total estimated time to complete all cards in seconds" do
       # TODO: hard coded time per card, will make configurable later
@@ -20,6 +66,16 @@ RSpec.describe Deck, type: :model do
       deck = create(:deck, card_count: 0)
 
       expect(deck.time_to_complete).to eq(0)
+    end
+  end
+
+  context "#unpublish!" do
+    it "unpublishes a quiz removing its published_at datetime" do
+      deck = create(:deck, published_at: Time.current)
+
+      expect {
+        deck.unpublish!
+      }.to change(deck, :published_at).to(nil)
     end
   end
 end
