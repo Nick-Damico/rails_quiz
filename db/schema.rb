9da98143050b9b89458a89c8d578f761f3442334
Nick-Damico <nickalan82@icloud.com>
2026-07-24 10:54:38 -0400

    feat(schema): add published_at datetime to decks/quizzes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_06_27_161252) do
+ActiveRecord::Schema[7.2].define(version: 2026_07_24_141319) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -88,6 +88,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_27_161252) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
+    t.datetime "published_at"
     t.index ["author_id"], name: "index_decks_on_author_id"
     t.index ["category_id"], name: "index_decks_on_category_id"
   end
@@ -116,6 +117,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_06_27_161252) do
     t.datetime "updated_at", null: false
     t.bigint "author_id", null: false
     t.bigint "category_id", null: false
+    t.datetime "published_at"
     t.index ["author_id"], name: "index_quizzes_on_author_id"
     t.index ["category_id"], name: "index_quizzes_on_category_id"
   end
