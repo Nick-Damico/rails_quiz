4c3c33017617e8b9e6b14303f57f212a0003948d
Nick-Damico <nickalan82@icloud.com>
2026-07-24 10:56:50 -0400

    feat(decks): add methods for publishing feature

--- app/models/quiz.rb
+++ app/models/quiz.rb
@@ -6,12 +6,30 @@ class Quiz < ApplicationRecord
   validates_presence_of :title
   validates_presence_of :author
 
+  PUBLISHABLE_QUESTION_COUNT = 5
   SECONDS_PER_QUESTION = 30.0
 
   # Future update with allow the Author to set the
   # estimated time per question. This will allow for more or
   # less time based on question difficulty.
+  def publish!
+    update_column(:published_at, Time.current) if publishable?
+  end
+
+  def publishable?
+    questions.count >= PUBLISHABLE_QUESTION_COUNT
+  end
+
+  def published?
+    published_at.present?
+  end
+
   def time_to_complete
     ActiveSupport::Duration.build(questions.count * SECONDS_PER_QUESTION).to_i
+  end
+
+  # NOTE: Eventually this will need some clean-up. We will need to notify users that had this as part of their study_plans.
+  def unpublish!
+    update_column(:published_at, nil)
   end
 end
--- spec/models/quiz_spec.rb
+++ spec/models/quiz_spec.rb
@@ -7,6 +7,53 @@ RSpec.describe Quiz, type: :model do
 
   it { should have_many(:questions) }
 
+  context "#publish!" do
+    let!(:quiz) { create(:quiz, questions_count: 5) }
+    it "publishes a publishale quiz by setting the published_at datetime" do
+      quiz = create(:quiz, questions_count: 5)
+
+      expect {
+        quiz.publish!
+      }.to change(quiz, :published_at).from(nil)
+    end
+
+    it "does not publish an unpublishable quiz" do
+      quiz = create(:quiz, questions_count: 0)
+
+      expect {
+      quiz.publish!
+    }.not_to change(quiz, :published_at)
+    end
+  end
+
+  context "#published?" do
+    it "returns false if published_at is nil" do
+      quiz = build(:quiz, published_at: nil)
+
+      expect(quiz).not_to be_published
+    end
+
+    it "returns true if published_at is present" do
+      quiz = build(:quiz, published_at: Time.current)
+
+      expect(quiz).to be_published
+    end
+  end
+
+  context "#publishable?" do
+    it "returns false if the quiz has less than 5 questions" do
+      quiz = create(:quiz, questions_count: 4)
+
+      expect(quiz).not_to be_publishable
+    end
+
+    it "returns true if the quiz has 5 or more questions" do
+      quiz = create(:quiz, questions_count: 5)
+
+      expect(quiz).to be_publishable
+    end
+  end
+
   context "#time_to_complete" do
     it "returns the total estimated time to complete all cards in seconds" do
       quiz = create(:quiz, questions_count: 2)
@@ -18,6 +65,16 @@ RSpec.describe Quiz, type: :model do
       quiz = create(:quiz, questions_count: 0)
 
       expect(quiz.time_to_complete).to eq(0)
+    end
+  end
+
+  context "#unpublish!" do
+    it "unpublishes a quiz removing its published_at datetime" do
+      quiz = create(:quiz, published_at: Time.current)
+
+      expect {
+        quiz.unpublish!
+      }.to change(quiz, :published_at).to(nil)
     end
   end
 end
