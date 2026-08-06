dec4e0193097573787bd3de0c113f8b15879b9e1
Nick-Damico <nickalan82@icloud.com>
2026-08-06 09:49:03 -0400

    refactor: public profile action searching decks or quizzes

--- app/controllers/dashboard/users_controller.rb
+++ app/controllers/dashboard/users_controller.rb
@@ -3,26 +3,22 @@ class Dashboard::UsersController < ApplicationController
   before_action :authorize_access!
   before_action :set_breadcrumbs, only: %i[public_profile]
 
+  TABS = %w[quiz deck].freeze
+
   def show; end
 
   def public_profile
     authorize([ :dashboard, current_user ])
 
-    params[:tab] ||= "quiz"
+    @tab = params[:tab]
+    @tab = "quiz" unless TABS.include?(@tab)
 
-    if params[:tab] == "quiz"
-      default_scope = @user.authored_quizzes
-                           .published
-                           .select(:id, :title, :description)
+    scope = @user.send("authored_#{@tab.pluralize}")
+                 .published
+                 .includes(:category)
+                 .select(:id, :title, :description, :category_id, categories: [ :name ])
 
-      search = QuizSearch.new(default_scope, params)
-    else
-      default_scope = @user.authored_decks
-                           .published
-                           .select(:id, :title, :description)
-
-      search = DeckSearch.new(default_scope, params)
-    end
+    search = "#{@tab.capitalize}Search".safe_constantize.new(scope, params)
 
     @pagy, @resources = pagy(:countish, search.query)
   end
