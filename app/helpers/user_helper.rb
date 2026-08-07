ce5eeb3af9a6521cbda18f6126969f0958fa93bc
Nick-Damico <nickalan82@icloud.com>
2026-08-07 09:18:08 -0400

    feat: set active classes on tab navigation selection

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -4,4 +4,14 @@ module UserHelper
 
     user.rank.titleize
   end
+
+  def public_profile_tab_classes(active_tab, current_tab)
+    base_styles = "inline-block p-4 border-b-2 rounded-t-lg"
+
+    if active_tab == current_tab
+      "#{base_styles} text-blue-600 border-blue-600"
+    else
+      "#{base_styles} border-transparent text-gray-500 hover:text-gray-600 hover:border-gray-300"
+    end
+  end
 end
