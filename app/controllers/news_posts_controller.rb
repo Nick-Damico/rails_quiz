7dc76cb542f8d195d8a19a688e78dbea70a9a681
Nick-Damico <nickalan82@icloud.com>
2026-08-16 09:58:18 -0400

    feat(news_post): add breadcrumbs

--- app/controllers/news_posts_controller.rb
+++ app/controllers/news_posts_controller.rb
@@ -1,5 +1,6 @@
 class NewsPostsController < ApplicationController
   before_action :get_news_post, only: %i[show]
+  before_action :set_breadcrumbs, only: %i[show]
 
   def index
     @news_posts = NewsPost.all
@@ -10,6 +11,11 @@ class NewsPostsController < ApplicationController
   end
 
   private
+
+    def set_breadcrumbs
+      add_breadcrumb("News Posts", root_path)
+      add_breadcrumb(@news_post.title, news_post_path(@news_post))
+    end
 
     def get_news_post
       @news_post = NewsPost.find_by_slug(params[:id])
