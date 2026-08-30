2023965e5c8c140bc5977108cb2a7e624956f26e
Nick-Damico <nickalan82@icloud.com>
2026-08-30 10:48:39 -0400

    feat(wip): initial UserSearch setup

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,7 +2,10 @@ class Admin::DashboardController < ApplicationController
   def show
     @user = current_user
     authorize @user
-    @pagy, @resources = pagy(:countish, User.all)
+    default_scope = policy_scope([ :admin, User ])
+    search = UserSearch.new(default_scope, params)
+
+    @pagy, @resources = pagy(:countish, search.query)
   end
 
   private
---
+++ app/queries/user_search.rb
@@ -0,0 +1,22 @@
+class UserSearch < ApplicationSearch
+  def filter_options
+    filters = {}
+    # filters[:categories] = categories
+    filters
+  end
+
+  def query
+    scope
+
+    # return scope unless params.dig(:category_ids).present?
+
+    # @scope = scope.where(category_id: params.dig(:category_ids))
+
+    # scope
+  end
+
+  private
+  # def categories
+  #   Category.select(:id, :name, :slug).all.order(:name)
+  # end
+end
