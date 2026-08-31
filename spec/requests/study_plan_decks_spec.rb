a96f093ded24f00aa337738ff2e22f47155c262a
Nick-Damico <nickalan82@icloud.com>
2026-08-31 12:01:23 -0400

    fix: merge conflict

--- spec/requests/study_plan_decks_spec.rb
+++ spec/requests/study_plan_decks_spec.rb
@@ -2,44 +2,71 @@
 require "rails_helper"
 
 RSpec.describe "StudyPlanDecks", type: :request do
-  let(:user) { create(:user) }
-  let(:study_plan) { create(:study_plan, user: user) }
-  let(:deck) { create(:deck) }
+    let(:user) { create(:user) }
+    let(:study_plan) { create(:study_plan, user: user) }
+    let(:deck) { create(:deck) }
 
-  before do
-    sign_in user
-  end
-
-  describe "POST /study_plan_decks" do
-    subject(:create_study_plan_deck) do
-      post study_plan_decks_path,
-           params: { study_plan_deck: { study_plan_id: study_plan.id, deck_id: deck.id } }
+    before do
+      sign_in user
     end
 
-    context "when the deck is successfully added to the study plan" do
-      it "creates a study plan deck association" do
-        expect { create_study_plan_deck }
-          .to change(StudyPlanDeck, :count).by(1)
+    describe "POST /study_plan_decks" do
+      subject(:create_study_plan_deck) do
+        post study_plan_decks_path,
+             params: { study_plan_deck: { study_plan_id: study_plan.id, deck_id: deck.id } }
       end
 
-      it "redirects to the study plan show page" do
-        create_study_plan_deck
+      context "when the deck is successfully added to the study plan" do
+        it "creates a study plan deck association" do
+          expect { create_study_plan_deck }
+            .to change(StudyPlanDeck, :count).by(1)
+        end
 
-        # expect(response).to render_template("users/study_plans/show")
-        expect(response).to redirect_to(user_study_plan_path(user, study_plan))
+        it "redirects to the study plan show page" do
+          create_study_plan_deck
+
+          # expect(response).to render_template("users/study_plans/show")
+          expect(response).to redirect_to(user_study_plan_path(user, study_plan))
+        end
+
+        it "sets a success flash message" do
+          create_study_plan_deck
+
+          expect(flash[:notice]).to eq("Deck was successfully added to your study plan.")
+        end
       end
 
-      it "sets a success flash message" do
-        create_study_plan_deck
+      context "when the deck is already in the study plan" do
+        before do
+          create(:study_plan_deck, study_plan: study_plan, deck: deck)
+        end
+        it "does not create another study plan deck association" do
+          expect { create_study_plan_deck }
+            .not_to change(StudyPlanDeck, :count)
+        end
 
-        expect(flash[:notice]).to eq("Deck was successfully added to your study plan.")
+        it "redirects the deck show page" do
+          create_study_plan_deck
+
+          expect(response).to redirect_to deck_path(deck)
+        end
+
+        it "sets an error flash message" do
+          create_study_plan_deck
+
+          expect(flash[:alert]).to eq(
+            "Deck already added to your study plan. Please select a different deck."
+          )
+        end
       end
     end
 
-    context "when the deck is already in the study plan" do
+    # FIX: Create condition where a deck cannot be added to the study plan
+    xcontext "when the deck cannot be added to the study plan" do
       before do
         create(:study_plan_deck, study_plan: study_plan, deck: deck)
       end
+
       it "does not create another study plan deck association" do
         expect { create_study_plan_deck }
           .not_to change(StudyPlanDeck, :count)
@@ -55,52 +82,35 @@ RSpec.describe "StudyPlanDecks", type: :request do
         create_study_plan_deck
 
         expect(flash[:alert]).to eq(
-          "Deck already added to your study plan. Please select a different deck."
+          "There was an issue adding this deck to your study plan. Refresh page and try again."
         )
       end
     end
   end
 
-  describe "DELETE /study_plan_deck/:id" do
-    context "successfully removing a deck from the study plan" do
-      let!(:study_plan_deck) { create(:study_plan_deck, study_plan: study_plan, deck: deck) }
+xdescribe "DELETE /study_plans/:study_plan_id/study_plan_decks/:id" do
+  let!(:study_plan_deck) do
+    create(:study_plan_deck, study_plan: study_plan, deck: deck)
+  end
 
-      subject(:delete_study_plan_deck) do
-        delete study_plan_deck_path(study_plan_deck)
-      end
+  subject(:destroy_study_plan_deck) do
+    delete study_plan_deck_path(study_plan_deck)
+  end
 
-      it "removes the deck from the study plan" do
-        expect { delete_study_plan_deck }.to change(StudyPlanDeck, :count).by(-1)
-      end
+  it "removes the deck from the study plan" do
+    expect { destroy_study_plan_deck }
+      .to change(StudyPlanDeck, :count).by(-1)
+  end
 
-      it "renders a success flash message" do
-        delete_study_plan_deck
+  it "renders the study plan show page" do
+    destroy_study_plan_deck
 
-        expect(flash[:notice]).to eq(
-          "Deck was successfully removed from your study plan."
-        )
-      end
-    end
+    expect(response).to render_template("users/study_plans/show")
+  end
 
-    context "when the deck cannot be removed" do
-      let!(:study_plan_deck) { create(:study_plan_deck, study_plan:, deck:) }
+  it "sets a success flash message" do
+    destroy_study_plan_deck
 
-      before do
-        allow_any_instance_of(StudyPlanDeck).to receive(:destroy).and_return(false)
-      end
-
-      it "does not remove the deck" do
-        expect { delete study_plan_deck_path(study_plan_deck) }
-          .not_to change(StudyPlanDeck, :count)
-      end
-
-      it "shows an error message" do
-        delete study_plan_deck_path(study_plan_deck)
-
-        expect(flash[:alert]).to eq(
-          "Unable to remove deck from study plan. Refresh page and try again."
-        )
-      end
-    end
+    expect(flash[:notice]).to eq("Deck removed from study plan.")
   end
 end
