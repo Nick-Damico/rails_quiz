ef5d55adaa7d103822c67e4f54611a6639fb0dd8
Nick-Damico <nickalan82@icloud.com>
2026-09-03 09:53:02 -0400

    feat: adjust controller logic for user, quiz and decks

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -2,11 +2,16 @@ class Admin::DashboardController < ApplicationController
   def show
     @user = current_user
     authorize @user
-    default_scope = policy_scope([ :admin, User ])
-    search = UserSearch.new(default_scope, params)
+    @tab = (params[:tab].presence || "user").capitalize
+    presenter = "Admin::#{@tab}Presenter".safe_constantize
+    search_class = "#{@tab}Search".safe_constantize
+    # TODO: Add Quiz & Deck admin policy
+    default_scope = policy_scope([ :admin, @tab.safe_constantize ])
+
+    search = search_class.new(default_scope, params)
 
     @pagy, objects = pagy(:countish, search.query)
-    @objects = objects.map { |user| Admin::UserPresenter.new(user) }
+    @objects = objects.map { |object| presenter.new(object) }
   end
 
   private
