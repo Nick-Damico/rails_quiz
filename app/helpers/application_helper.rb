936cd526374c77ff1be51f7436e86a48520f605d
Nick-Damico <nickalan82@icloud.com>
2026-09-04 10:32:04 -0400

    refactor: rename tab_classes from public_profile_*

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,4 +4,14 @@ module ApplicationHelper
       yield if block_given?
     end
   end
+
+  def tab_classes(active_tab, current_tab)
+    base_styles = "inline-block p-4 border-b-2 rounded-t-lg"
+
+    if active_tab.downcase == current_tab.downcase
+      "#{base_styles} text-blue-600 border-blue-600"
+    else
+      "#{base_styles} border-transparent text-gray-500 hover:text-gray-600 hover:border-gray-300"
+    end
+  end
 end
