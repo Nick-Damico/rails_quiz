dbad103255677b5186bbd11d86aac60e249ed8ff
Nick-Damico <nickalan82@icloud.com>
2026-09-17 09:12:54 -0400

    feat(schema): add hidden to decks

---
+++ db/migrate/20260917125346_add_column_hidden_to_decks.rb
@@ -0,0 +1,5 @@
+class AddColumnHiddenToDecks < ActiveRecord::Migration[7.2]
+  def change
+    add_column :decks, :hidden, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2026_09_15_140832) do
+ActiveRecord::Schema[7.2].define(version: 2026_09_17_125346) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -93,6 +93,7 @@ ActiveRecord::Schema[7.2].define(version: 2026_09_15_140832) do
     t.datetime "updated_at", null: false
     t.bigint "category_id", null: false
     t.datetime "published_at"
+    t.boolean "hidden"
     t.index ["author_id"], name: "index_decks_on_author_id"
     t.index ["category_id"], name: "index_decks_on_category_id"
   end
